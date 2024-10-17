ff96a55b1afb9bd740364dce22e2542724c906c3
very97 <very97@naver.com>
2024-10-17 15:27:23 +0900

    2024-10-17 과제 완료

--- todo_backend/app.js
+++ todo_backend/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
 
 const app = express();
+app.use(bodyParser.json());
+app.use("/api", indexRouter);
 
 //bodyParser setting
 app.use(bodyParser.json());
 
-const mongoURI = "mongodb://localhost:27017/todo-demo";
+const mongoURI = `mongodb://localhost:27017/todo-demo`;
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => {
