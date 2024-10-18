9689d6e052054ae47f98cb6adee2ed4bbb40678b
very97 <very97@naver.com>
2024-10-18 14:47:46 +0900

    백엔드 AWS 배포 준비 완료

--- todo_backend/app.js
+++ todo_backend/app.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+//console.log("mongoouri", MONGODB_URI_PROD);
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -13,7 +16,8 @@ app.use("/api", indexRouter);
 //bodyParser setting
 app.use(bodyParser.json());
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = MONGODB_URI_PROD;
+
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => {
