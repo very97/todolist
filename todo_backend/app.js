c7c167282aeb1e73a7efd57c80ec390ebfe92332
very97 <very97@naver.com>
2024-10-18 14:00:06 +0900

    풀스택 완성

--- todo_backend/app.js
+++ todo_backend/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 app.use("/api", indexRouter);
 
 //bodyParser setting
